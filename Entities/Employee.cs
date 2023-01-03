03017708e4fa3f2a216f5785909c511a104615e5
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2023-01-03 11:18:29 -0300

    Created 2 classes. OutsourceEmployee and Employee

---
+++ Entities/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercResolvidoInheritance.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double valuePerHour { get; set; }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            this.valuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return valuePerHour * Hours;
+        }
+    }
+}
---
+++ Entities/OutsourcedEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercResolvidoInheritance.Entities
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public double AdditionalCharge { get; set; }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
