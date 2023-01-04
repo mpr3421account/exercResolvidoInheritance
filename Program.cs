8a4048e0199590b16225ba3f6c5d61e84f27136f
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2023-01-04 09:33:26 -0300

    Final

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿/*Uma empresa possui funcionários próprios e terceirizados.
+﻿using exercResolvidoInheritance.Entities;
+using System.Globalization;
+
+/*Uma empresa possui funcionários próprios e terceirizados.
 Para cada funcionário, deseja-se registrar nome, horas
 trabalhadas e valor por hora. Funcionários terceirizados
 possuem ainda uma despesa adicional.
@@ -13,3 +16,40 @@ funcionário na mesma ordem em que foram digitados.
 Construa o programa conforme projeto ao lado. Veja
 exemplo na próxima página.
 */
+List<Employee> list = new List<Employee>();
+
+Console.Write("Enter the number of employees: ");
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"Employee #{i} data: ");
+    Console.Write("Outsourced (y/n)? ");
+    char c = char.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Hours: ");
+    int hours = int.Parse(Console.ReadLine());
+    Console.Write("Value per hour: ");
+    double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    if(c == 'y')
+    {
+        Console.Write("Additional charge: ");
+        double additionalCharge = double.Parse(Console.ReadLine() , CultureInfo.InvariantCulture);
+        list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+    }
+    else
+    {
+        list.Add(new Employee(name, hours, valuePerHour));
+    }
+
+    
+}
+
+Console.WriteLine();
+Console.WriteLine("PAYMENTS:");
+
+foreach (Employee emp in list)
+{
+    Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+}
